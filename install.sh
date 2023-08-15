c6f007802a36f58fe403049aa801951a711d36dd
Ghost 0day <83251831+gt0day@users.noreply.github.com>
2023-08-15 05:33:36 +0300

    v1.0

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "AnonGT Installer"
 
 sudo apt update
 sudo apt install -y tor iptables bleachbit nyx xterm
-sudo mv ../anongt /usr/share
+sudo mv ../AnonGT /usr/share/anongt
 sudo touch anongt /usr/bin
 sudo echo 'python3 /usr/share/anongt/anongt.py $1' > /usr/bin/anongt
 sudo chmod +x /usr/share/anongt/anongt.py
